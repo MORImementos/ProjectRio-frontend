e64613b827fb788453a1cc2d19a97e7f1ad6f512
Micah Russell <108246578+thknkg@users.noreply.github.com>
2023-07-17 03:42:03 -0500

    update

--- src/routes/tag_set/create/+page.svelte
+++ src/routes/tag_set/create/+page.svelte
@@ -2,28 +2,38 @@
     import type { PageData } from './$types';
     import { superForm } from 'sveltekit-superforms/client';
     import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
-  
+    import { UNCATEGORIZED_ENDPOINTS, GET } from "$lib/constants";
+    import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
+
     export let data: PageData;
   
     // Client API:
     const { form, errors, constraints, enhance } = superForm(data.form);
-  
+    import { onMount } from 'svelte';
+
+    let tags = [];
+    onMount(async () => {
+      const response = await GET(UNCATEGORIZED_ENDPOINTS.TAG_LIST);
+      const res = await response;
+      tags = res.Tags;
+      console.log(tags);
+    });
   </script>
-    <SuperDebug data={$form} />
+<!--    <SuperDebug data={$form} />-->
 
-  {#if data}
-  <!-- <div>{data.username}</div> -->
-  {/if}
+{#if tags}
   <!-- <SuperDebug data={$form} /> -->
-  <div class="flex items-center justify-center h-screen ">
-    <div class="p-4 md:p-10 flex bg-gradient-to-br variant-gradient-primary-secondary w-[80%] h-full rounded-container-token shadow-2xl space-y-10">
-  <form method="POST" class="flex card flex-col justify-center items-center mx-auto transition-[width] duration-200 w-[80%] h-full shadow-2xl" use:enhance>
+  <div class="flex items-center justify-center">
+    <div class="p-4 md:p-10 flex bg-gradient-to-br variant-gradient-primary-secondary rounded-container-token shadow-2xl space-y-10">
+  <form method="POST" class="flex card flex-col justify-center items-center mx-auto transition-[width] duration-200 shadow-2xl" use:enhance>
 
-    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[20%]">
+<!--    <div class="input-group">-->
 
-      <label for="name">name</label>
+    <div class="input-group card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
+
+      <label class="label" for="name">Name</label>
       <input
-      class="text-primary-50-900-token"
+      class="input text-token"
       type="text"
       name="name"
   
@@ -32,22 +42,25 @@
       {...$constraints.name} />
       {#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
   </div>
-    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[20%]">
+<!--      </div>-->
+    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
 
-      <label for="desc">description</label>
+      <label class="label" for="desc">Description</label>
       <textarea
-      class="text-primary-50-900-token"
+      class="textarea text-token"
+          row="4"
           name="desc"
+          placeholder="Enter description of this game mode."
           aria-invalid={$errors.desc ? 'true' : undefined}
           bind:value={$form.desc}
           {...$constraints.desc}></textarea>
       {#if $errors.desc}<span class="invalid">{$errors.desc}</span>{/if}
   </div>
-  <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[20%]">
+  <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
 
-    <label for="type">type</label>
+    <label class="label" for="type">Type</label>
     <select
-      class="text-primary-50-900-token"
+      class="select text-token"
       name="type"
       aria-invalid={$errors.type ? 'true' : undefined}
       bind:value={$form.type}
@@ -61,11 +74,10 @@
     </select>
     {#if $errors.type}<span class="invalid">{$errors.type}</span>{/if}
 </div>
-    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[20%]">
+    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
 
-    <label for="community_name">community name</label>
-    <input
-    class="text-primary-50-900-token"
+    <label class="label" for="community_name">community name</label>
+    <input class="input text-token"
     type="text"
     name="community_name"
 
@@ -75,20 +87,40 @@
     {#if $errors.community_name}<span class="invalid">{$errors.community_name}</span>{/if}
 </div>
 
-    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[15%]">
-    <label for="tags">tags</label>
-    <input
-      type="checkbox"
-      name="tags"
-      aria-invalid={$errors.tags ? 'true' : undefined}
-      bind:checked={$form.tags}
-      {...$constraints.tags} />
-    {#if $errors.tags}<span class="invalid">{$errors.tags}</span>{/if}
-  </div>
-  <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[15%]">
+<!--    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[15%]">-->
+<!--    <label for="tags">tags</label>-->
+<!--    <input-->
+<!--      type="checkbox"-->
+<!--      name="tags"-->
+<!--      aria-invalid={$errors.tags ? 'true' : undefined}-->
+<!--      bind:checked={$form.tags}-->
+<!--      {...$constraints.tags} />-->
+<!--    {#if $errors.tags}<span class="invalid">{$errors.tags}</span>{/if}-->
+<!--  </div>-->
 
-    <label for="tag_set_id">tagset id</label>
-    <input
+    <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
+      <div class="tags">
+      <label class="label" for="tags">Tags</label>
+      <select class="select" multiple name="tags" bind:value={$form.tags}>
+        {#each tags as tag}
+          <span class="chip variant-soft hover:variant-filled">
+            <span>(icon)</span>
+            <span>Action</span>
+          </span>
+          <option value={tag.name}>{tag.name}</option>
+        {/each}
+
+      </select>
+      {#if $errors.tags}<span class="invalid">{$errors.tags}</span>{/if}
+      </div>
+      <div class="chips">
+
+      </div>
+    </div>
+  <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
+
+    <label class="label" for="tag_set_id">tagset id</label>
+    <input class="input variant-form-material"
       type="checkbox"
       name="tag_set_id"
       aria-invalid={$errors.tag_set_id ? 'true' : undefined}
@@ -96,38 +128,36 @@
       {...$constraints.tag_set_id} />
     {#if $errors.tag_set_id}<span class="invalid">{$errors.tag_set_id}</span>{/if}
 </div>
-<div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[15%]">
 
-  <label for="start_date">start date</label>
-  <input
-    type="date"
-    name="start_date"
-    aria-invalid={$errors.start_date ? 'true' : undefined}
-    bind:value={$form.start_date}
-    {...$constraints.start_date} />
-  {#if $errors.start_date}<span class="invalid">{$errors.start_date}</span>{/if}
-</div>
-<div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[15%]">
+    <div class="input-group grid-cols-[auto_1fr_auto] input-group-divider">
+  <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
+    <label class="label" for="start_date">start date</label>
+    <input class="input variant-form-material"
+      type="date"
+      name="start_date"
+      aria-invalid={$errors.start_date ? 'true' : undefined}
+      bind:value={$form.start_date}
+      {...$constraints.start_date} />
+    {#if $errors.start_date}<span class="invalid">{$errors.start_date}</span>{/if}
+  </div>
+  <div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
 
-  <label for="end_date">end date</label>
-  <input
-    type="date"
-    name="end_date"
-    aria-invalid={$errors.end_date ? 'true' : undefined}
-    bind:value={$form.end_date}
-    {...$constraints.end_date} />
-  {#if $errors.end_date}<span class="invalid">{$errors.end_date}</span>{/if}
+    <label class="label" for="end_date">end date</label>
+    <input class="input variant-form-material"
+      type="date"
+      name="end_date"
+      aria-invalid={$errors.end_date ? 'true' : undefined}
+      bind:value={$form.end_date}
+      {...$constraints.end_date} />
+    {#if $errors.end_date}<span class="invalid">{$errors.end_date}</span>{/if}
+  </div>
 </div>
-<div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl w-[80%] h-[10%]">
+<div class="card flex flex-col p-4 m-2 text-token space-y-4 shadow-2xl">
     
-<button>Submit</button></div>
+<button class="btn">Submit</button></div>
   </form>
   </div>
   </div>
-  
-  <style>
-    .invalid {
-      color: red;
-    }
-  </style>
+  {/if}
+
   
--- src/routes/user/community/+page.svelte
+++ src/routes/user/community/+page.svelte
@@ -1,16 +1,84 @@
 <script lang="ts">
     import { GET, USER_ENDPOINTS } from "$lib/constants";
-    import { onMount } from "svelte";
-
+    import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
+    import {titleCase} from "$lib/utils";
     let data;
-    onMount(async () => {
-        const response = await GET(USER_ENDPOINTS.USER_COMMUNITY)
+    let username = '';
+
+    async function handleClick() {
+        const response = await GET(USER_ENDPOINTS.USER_COMMUNITY, `?username=${username}`)
         const res = await response;
         console.log(res)
-        data = res;
-    })
+        data = res.communities;
+    }
+
+    // $: data;
 </script>
 
+{#if !(data)}
+
+    <h3 class="h3 flex flex-auto justify-center align-middle text-center">Enter username to view user communities.</h3>
+    <div class="input-group flex m-2 justify-center align-middle w-[25%]">
+        <span class="p-2 justify-center align-middle">
+            <input class="input justify-center align-middle" type="text" placeholder="Enter username." bind:value={username} />
+        </span>
+        <span class="p-2 justify-center align-middle">
+            <button class="btn variant-filled-success justify-center align-middle" on:click={handleClick}><span class="label">Submit</span></button>
+       </span>
+    </div>
+{/if}
+
 {#if data}
-    <div>{data}</div>
+    <div class="table-container flex flex-col">
+        <div class="flex flex-auto">
+            <h1 class="h1 flex-auto flex">{username} Communities</h1>
+            <button class="btn variant-filled-success justify-center" on:click={}>Click to enter a different username.</button>
+        </div>
+        <table class="table flex-auto">
+            <thead>
+            <tr>
+                <th>Name</th>
+                {#each Object.keys(data[0]) as key}
+                    {#if !(key === "name")}
+                        <th>{titleCase(key)}</th>
+                    {/if}
+                {/each}
+            </tr>
+            </thead>
+            <tbody>
+            {#each data as item}
+                <tr>
+                    <td>{item.name}</td>
+                    {#each Object.keys(data[0]) as key}
+                        {#if !(key === "name")}
+                            {#if Object.keys(item).includes(key)}
+                                {#if (key === "date_created")}
+                                    <td>{new Date(item[key] * 1000).toLocaleDateString()}</td>
+                                {:else if (key === "desc")}
+                                    <td>
+                                        <Accordion>
+                                            <AccordionItem>
+                                                <svelte:fragment slot="summary">View Description</svelte:fragment>
+                                                <svelte:fragment slot="content">
+                                                    <div class="card" style="white-space: pre-wrap; max-width: none;">{item[key]}</div>
+
+                                                </svelte:fragment>
+
+                                            </AccordionItem>
+                                        </Accordion>
+                                    </td>
+                                {:else}
+                                    <td>{item[key]}</td>
+                                {/if}
+
+                            {:else}
+                                <td></td>
+                            {/if}
+                        {/if}
+                    {/each}
+                </tr>
+            {/each}
+            </tbody>
+        </table>
+    </div>
 {/if}
